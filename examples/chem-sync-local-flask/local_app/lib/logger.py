237d64dfc3b5e6988badf7aad1cf6b59512507ef
William Bowen <william@benchling.com>
2024-01-05 19:31:35 -0800

    Update outdated comment

--- examples/chem-sync-local-flask/local_app/lib/logger.py
+++ examples/chem-sync-local-flask/local_app/lib/logger.py
@@ -6,8 +6,7 @@ logging.basicConfig(format="%(name)s - %(asctime)s - {%(pathname)s:%(lineno)d} -
                     level=logging.WARNING)
 
 
-# Get a logger for our App, with DEBUG enabled by default specifically for our logging
-# but not other libraries
+# Get a logger for our App, with a level enabled specifically for our logging but not other libraries
 def get_logger(name: str = "benchling-app") -> logging.Logger:
     level = os.environ.get("BENCHLING_APP_LOG_LEVEL", "INFO").upper()
     logger = logging.getLogger(name)
