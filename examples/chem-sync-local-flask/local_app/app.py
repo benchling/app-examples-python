92c35c4ac56d347465f968576d534fbd31f85124
William Bowen <william@benchling.com>
2024-03-27 14:00:08 -0700

    Update webhook verification to use new SDK and verify() method

--- examples/chem-sync-local-flask/local_app/app.py
+++ examples/chem-sync-local-flask/local_app/app.py
@@ -1,9 +1,10 @@
 from threading import Thread
 
-from benchling_sdk.apps.helpers.webhook_helpers import verify_app_installation
+from benchling_sdk.apps.helpers.webhook_helpers import verify
 from flask import Flask, request
 
 from local_app.benchling_app.handler import handle_webhook
+from local_app.benchling_app.setup import app_definition_id
 from local_app.lib.logger import get_logger
 
 logger = get_logger()
@@ -20,11 +21,11 @@ def create_app() -> Flask:
     @app.route("/1/webhooks/<path:target>", methods=["POST"])
     def receive_webhooks(target: str) -> tuple[str, int]:  # noqa: ARG001
         # For security, don't do anything else without first verifying the webhook
-        app_id = request.json["app"]["id"]  # type: ignore[index]
+        app_def_id = app_definition_id()
 
         # Important! To verify webhooks, we need to pass the body as an unmodified string
         # Flask's request.data is bytes, so decode to string. Passing bytes or JSON won't work
-        verify_app_installation(app_id, request.data.decode("utf-8"), request.headers)
+        verify(app_def_id, request.data.decode("utf-8"), request.headers)
 
         logger.debug("Received webhook message: %s", request.json)
         # Dispatch work and ACK webhook as quickly as possible
