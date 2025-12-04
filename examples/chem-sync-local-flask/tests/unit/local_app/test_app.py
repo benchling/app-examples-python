65744dd6aa6481a076146f7a29df3cb45cf2479e
Danny Shin <danny.shin@ML-danny.shin-HY9R9R2VM9>
2025-12-04 10:46:23 -0800

    completed localhost compatability

--- examples/chem-sync-local-flask/tests/unit/local_app/test_app.py
+++ examples/chem-sync-local-flask/tests/unit/local_app/test_app.py
@@ -28,14 +28,8 @@ def client(app: Flask) -> FlaskClient:
 class TestApp:
 
     @patch("local_app.app._enqueue_work")
-    @patch("local_app.app.app_definition_id")
-    @patch("local_app.app.verify")
-    def test_app_receive_webhook(
-            self, mock_verify, mock_app_definition_id, mock_enqueue_work, client,
-    ) -> None:
+    def test_app_receive_webhook(self, mock_verify_app_installation, client) -> None:
         webhook = load_webhook_json(_TEST_FILES_PATH / "canvas_initialize_webhook.json")
         response = client.post("1/webhooks/canvas", json=webhook.to_dict())
         assert response.status_code == 200
-        mock_verify.assert_called_once()
-        mock_app_definition_id.assert_called_once()
-        mock_enqueue_work.assert_called_once()
+        mock_verify_app_installation.assert_called_once()
